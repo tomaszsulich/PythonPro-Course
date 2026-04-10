b7fdbaf8fcc9ff74639fa0cdd69d1d53fa534633
tomaszsulich <tomeksulich5@gmail.com>
2026-04-10 17:15:40 +0200

    Add files via upload

---
+++ homework/lesson1/task1_about_me.py
@@ -0,0 +1,4 @@
+print("Nazywam się Tomasz Sulich.")
+print("Interesuję się analizą danych i programowaniem, ale także szeroko pojętymi podróżami.")
+print("Uczę się Pythona, aby rozwijać się w kierunku Data Science.")
+print("Moim celem jest zdobycie pierwszej pracy w IT.")
