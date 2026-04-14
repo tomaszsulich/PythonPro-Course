7d79e1173c8c0655f2291a97ac88e83027b977d7
tomaszsulich <tomeksulich5@gmail.com>
2026-04-14 21:51:23 +0200

    Add files via upload
    Add lesson 3 homework

---
+++ homework/lesson3/task2_BMI_calculator.py
@@ -0,0 +1,4 @@
+wzrost = float(input("Podaj wzrost [m]: "))
+waga = float(input("Podaj wagę [kg]: "))
+bmi = waga / (wzrost * wzrost)
+print(f"Twoje BMI wynosi {bmi:.4f}.")
---
+++ homework/lesson3/task4_age_calculator.py
@@ -0,0 +1,6 @@
+imie = input("Jak masz na imię? ")
+rok_urodzenia = int(input("W którym roku się urodziłeś? "))
+
+wiek = 2025 - rok_urodzenia
+
+print(f"Cześć, {imie}! W 2025 roku będziesz mieć około {wiek} lat.")
