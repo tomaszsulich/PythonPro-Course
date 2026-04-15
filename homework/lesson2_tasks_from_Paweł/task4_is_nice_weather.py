e65f62430596b42327d7c9d67da9a90e079d0613
tomaszsulich <tomeksulich5@gmail.com>
2026-04-15 14:51:02 +0200

    Add files via upload
    Update task 4 task

--- homework/lesson2_tasks_from_Paweł/task4_is_nice_weather.py
+++ homework/lesson2_tasks_from_Paweł/task4_is_nice_weather.py
@@ -12,14 +12,28 @@ nice_days = 0
 for i in range(len(temps)):
     temp = temps[i]
     rain = rains[i]
-    weather = {
-        'temp': temp,
-        'rain': rain
-    }
-    weather_data.append(weather)
+    weather_data.append({"temp": temps[i], "rain": rains[i]})
 
 for day in weather_data:
         if is_nice_weather(day['temp'], day['rain']):
             nice_days += 1
         
 print(f"Liczba pięknych dni wynosi {nice_days}.")
+
+# ROZWIĄZANIE OD PAWŁA
+# def is_nice_weather(temp: int, rain: bool) -> bool:
+#     if 15 <= temp <= 25 and not rain:
+#         return True
+#     else:
+#         return False
+        
+# temps = [12, 15, 14, 18, 20, 19, 24, 21, 18, 17, 24]
+# rains = [False, False, True, False, True, False, False, True, True, False, False]
+# weather_data = []
+# nice_days = 0
+
+# for t, r in zip(temps, rains):
+#     weather_data.append({"temp": t, "rain": r})
+#     nice_days += int(is_nice_weather(t, r))
+        
+# print(f"Liczba pięknych dni wynosi {nice_days}.")
