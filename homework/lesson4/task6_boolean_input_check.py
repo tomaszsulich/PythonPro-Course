a421f8101ea5ee6432d309867d0d4d71fd19ee02
tomaszsulich <tomeksulich5@gmail.com>
2026-04-15 18:41:09 +0200

    Add files via upload
    Added comment regarding bool

--- homework/lesson4/task6_boolean_input_check.py
+++ homework/lesson4/task6_boolean_input_check.py
@@ -1,5 +1,6 @@
 tekst = input("Wpisz dowolny tekst, pusty też OK: ")
 
+# bool tak naprawdę jest tu zbędny, ponieważ pusty string = False, niepusty = True
 if bool(tekst):
     print(f"Wprowadzony tekst to: {tekst}")
 else:
