22e88d7b98575cac1d3f7b73b84d610e2af46d61
tomaszsulich <tomeksulich5@gmail.com>
2026-04-23 19:36:13 +0200

    Update task8_dog_years.py

--- homework/lesson4/task8_dog_years.py
+++ homework/lesson4/task8_dog_years.py
@@ -32,8 +32,8 @@ else:
 # wiek = int(input("Podaj wiek psa: "))
 
 # if wiek == 1:
-#     print(f"Psa ma {15} lat")
+#     print(f"Pies ma {15} lat")
 # elif wiek == 2:
-#     print(f"Psa ma {15 + 9} lat")
+#     print(f"Pies ma {15 + 9} lat")
 # else:
-#     print("Psa ma", 15 + 9 + (wiek - 2) * 5, "lat")
+#     print("Pies ma", 15 + 9 + (wiek - 2) * 5, "lat")
