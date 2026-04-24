6b5d4f78ee7c3bc93a6fd0e3be26fddfbd838040
tomaszsulich <tomeksulich5@gmail.com>
2026-04-24 15:30:52 +0200

    Add files via upload
    Update task 10

--- homework/lesson6/task10_password_validation.py
+++ homework/lesson6/task10_password_validation.py
@@ -13,18 +13,92 @@ def sprawdz_haslo(haslo: str) -> bool:
     Returns:
         bool: True, jeśli hasło spełnia wszystkie warunki, w przeciwnym razie False
     """
-    co_najmniej_jedna_wielka = False
-    co_najmniej_jedna_cyfra = False
+    ma_wielka = False
+    ma_cyfre = False
     
     for znak in haslo:
         if znak.isupper():
-            co_najmniej_jedna_wielka = True
+            ma_wielka = True
         if znak.isdigit():
-            co_najmniej_jedna_cyfra = True
+            ma_cyfre = True
             
-    return len(haslo) >= 8 and co_najmniej_jedna_wielka and co_najmniej_jedna_cyfra
+    return len(haslo) >= 8 and ma_wielka and ma_cyfre
 
 print(sprawdz_haslo("Haslo123")) # True
 print(sprawdz_haslo("haslo123")) # False (brak wielkiej litery)
 print(sprawdz_haslo("Hasloabc")) # False (brak cyfry)
 print(sprawdz_haslo("Has1")) # False (za krótkie)
+
+# WERSJA Z NADPISYWANIEM WARTOŚCI TYLKO RAZ - SZYBSZE DZIAŁANIE
+# def sprawdz_haslo(haslo: str) -> bool:
+#     """
+#     Sprawdza, czy hasło spełnia podstawowe wymagania bezpieczeństwa.
+    
+#     Warunki:
+#     - co najmniej 8 znaków
+#     - przynajmniej jedna wielka litera
+#     - przynajmniej jedna cyfra
+    
+#     Args:
+#         haslo (str): hasło do sprawdzenia
+        
+#     Returns:
+#         bool: True, jeśli hasło spełnia wszystkie warunki, w przeciwnym razie False
+#     """
+#     if len(haslo) < 8:
+#         return False
+    
+#     ma_wielka = False
+#     ma_cyfre = False
+    
+#     for znak in haslo:
+#         if not ma_wielka and znak.isupper():
+#             ma_wielka = True
+#         if not ma_cyfre and znak.isdigit():
+#             ma_cyfre = True
+#         if ma_wielka and ma_cyfre:
+#             return True
+
+#     return False
+    
+# print(sprawdz_haslo("Haslo123")) # True
+# print(sprawdz_haslo("haslo123")) # False (brak wielkiej litery)
+# print(sprawdz_haslo("Hasloabc")) # False (brak cyfry)
+# print(sprawdz_haslo("Has1")) # False (za krótkie)
+
+# WERSJA Z ENUMERATE, KTÓRE JEST KORZYSTNIEJSZE, GDY OPERUJEMY NA OGROMNEJ ILOŚCI DANYCH,
+# GDY LEN() JEST KOSZTOWNY I/ALBO GDY I TAK MUSIMY PRZEJŚĆ PO WSZYSTKICH ELEMENTACH
+# def sprawdz_haslo(haslo: str) -> bool:
+#     """
+#     Sprawdza, czy hasło spełnia podstawowe wymagania bezpieczeństwa.
+    
+#     Warunki:
+#     - co najmniej 8 znaków
+#     - przynajmniej jedna wielka litera
+#     - przynajmniej jedna cyfra
+    
+#     Args:
+#         haslo (str): hasło do sprawdzenia
+        
+#     Returns:
+#         bool: True, jeśli hasło spełnia wszystkie warunki, w przeciwnym razie False
+#     """
+#     ma_wielka = False
+#     ma_cyfe = False
+#     dlugosc = 0
+    
+#     for dlugosc, znak in enumerate(haslo, start=1):
+#         if not ma_wielka and znak.isupper():
+#             ma_wielka = True
+#         if not ma_cyfre and znak.isdigit():
+#             ma_cyfre = True
+            
+#         if dlugosc >= 8 and ma_wielka and ma_cyfre:
+#             return True
+        
+#         return False
+
+# print(sprawdz_haslo("Haslo123")) # True
+# print(sprawdz_haslo("haslo123")) # False (brak wielkiej litery)
+# print(sprawdz_haslo("Hasloabc")) # False (brak cyfry)
+# print(sprawdz_haslo("Has1")) # False (za krótkie)
