d39bd78239ab0b56e3ed1456162d828a57711592
tomaszsulich <tomeksulich5@gmail.com>
2026-04-24 15:30:10 +0200

    Add files via upload
    Update task 8

--- homework/lesson6/task8_creating_profile.py
+++ homework/lesson6/task8_creating_profile.py
@@ -16,3 +16,18 @@ profil2 = stworz_profil("Wojtek", wiek = 32, plec = "mężczyzna", uczelnia = "S
 
 wyswietl_profil(profil1)
 wyswietl_profil(profil2)
+
+# WERSJA Z ROZPAKOWYWANIEM SŁOWNIKA ZAMIAST PĘTLI FOR W FUNKCJI stworz_profil
+# def stworz_profil(imie, **dane_dodatkowe):
+#     return {"imię": imie, **dane_dodatkowe}
+
+# def wyswietl_profil(profil):
+#     for klucz, wartosc in profil.items():
+#         print(f"{klucz}: {wartosc}")
+#     print()
+
+# profil1 = stworz_profil("Anna", wiek = 45, miasto = "Łódź", uczelnia = "UKSW")
+# profil2 = stworz_profil("Wojtek", wiek = 32, plec = "mężczyzna", uczelnia = "SGGW", miasto = "Warszawa")
+
+# wyswietl_profil(profil1)
+# wyswietl_profil(profil2)
