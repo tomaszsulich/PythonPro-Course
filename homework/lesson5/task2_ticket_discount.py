762f2f6565a7504a44e4c4b695d248f8148e696e
tomaszsulich <tomeksulich5@gmail.com>
2026-04-24 15:36:43 +0200

    Add files via upload
    Update task 2

--- homework/lesson5/task2_ticket_discount.py
+++ homework/lesson5/task2_ticket_discount.py
@@ -5,10 +5,24 @@ czy_student = input("Czy jesteś studentem (tak/nie)? ").lower()
 
 if wiek < 0:
     print("Wiek nie może być ujemny!")
-if wiek >= 0 and (wiek < 18 or czy_student == "tak"):
+elif wiek >= 0 and (wiek < 18 or czy_student == "tak"):
     cena_biletu /= 2
-if wiek >= 0:
     print(f"Musisz zapłacić {cena_biletu:.2f} PLN." )
+else:
+    print(f"Musisz zapłacić {cena_biletu:.2f} PLN.")
+    
+# WERSJA CZYSTSZA, W KTÓREJ "UŻYJ AND I OR" TRAKTUJEMY JAKO SUGESTIĘ, A NIE WYMÓG
+# cena_biletu = 100
+
+# wiek = float(input("Ile masz lat? "))
+# czy_student = input("Czy jesteś studentem (tak/nie)? ").lower()
+
+# if wiek < 0:
+#     print("Wiek nie może być ujemny!")
+# elif wiek < 18 or czy_student == "tak":
+#     cena_biletu /= 2
+    
+# print(f"Musisz zapłacić {cena_biletu:.2f} PLN.")
 
 # WERSJA REALNA, NIE POD ZADANIE
 # cena_biletu = 100
