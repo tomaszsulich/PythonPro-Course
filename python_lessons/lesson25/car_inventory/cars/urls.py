cf2e91a71c0dc1d88a57d954d284ae07b3ed62f6
tomek <tomeksulich5@gmail.com>
2026-07-13 18:41:31 +0200

    feat: demonstrate DRF routing and REST client requests

--- python_lessons/lesson25/car_inventory/cars/urls.py
+++ python_lessons/lesson25/car_inventory/cars/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
+# user urls
 urlpatterns = [
     # ... inne ścieżki
-    path('register/', views.register, name='register'),
-    path('', views.home, name='home'),
+    path('cars/', views.register, name='register'),
+    path('addcar', views.home, name='home'),
     path('profile/', views.profile, name='profile')
 ]
