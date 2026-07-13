1c3d2310037f13c36fbe21773c3fe395ec7a2e90
tomek <tomeksulich5@gmail.com>
2026-07-13 17:34:25 +0200

    feat: add authentication improvements and staff user management

--- python_lessons/lesson25/car_inventory/car_inventory/urls.py
+++ python_lessons/lesson25/car_inventory/car_inventory/urls.py
@@ -17,12 +17,24 @@ Including another URLconf
 from django.contrib import admin
 from cars import views as car_views
 from django.contrib.auth import views as auth_views
+
 from django.urls import path, include
 
 urlpatterns = [
     # ...
     path('register/', car_views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('login/', 
+         auth_views.LoginView.as_view(template_name='users/login.html'), 
+         name='login'),
+    path('logout/', 
+         auth_views.LogoutView.as_view(template_name='users/logout.html'), 
+         name='logout'),
+    path('password-change/', auth_views.PasswordChangeView.as_view(template_name='password_change_form.html'),
+         name='password-change'),
+    path('password-change/done/',
+         auth_views.PasswordChangeDoneView.as_view(
+             template_name='password_change_done.html'),
+         name='password_change_done'),
     path('', include('cars.urls')),
+    path('users/', car_views.user_list, name='users-list')
 ]
--- python_lessons/lesson25/car_inventory/cars/views.py
+++ python_lessons/lesson25/car_inventory/cars/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-# Create your views here.
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
 from django.contrib import messages
+
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.models import User
+from django.contrib.auth import login as auth_login
+
 
 def register(request):
     # Sprawdzamy, czy metoda żądania to POST (wysyłka formularza)
@@ -12,10 +16,14 @@ def register(request):
         form = CustomUserCreationForm(request.POST)
         # Sprawdzamy, czy formularz jest poprawny
         if form.is_valid():
-            form.save() # Zapisujemy użytkownika w bazie danych
+            user = form.save() # Zapisujemy użytkownika w bazie danych
             username = form.cleaned_data.get('username')
             # Wyświetlamy komunikat o sukcesie
-            messages.success(request, f'Konto dla {username} zostało utworzone! Możesz się teraz zalogować.')
+            auth_login(request, user)
+            messages.success(
+                request, 
+                f'Konto dla {username} zostało utworzone! Możesz się teraz zalogować.'
+            )
             return redirect('login') # Przekierowujemy na stronę logowania
     else:
         # Jeśli metoda to GET, tworzymy pusty formularz
@@ -24,9 +32,22 @@ def register(request):
     # Renderujemy szablon z formularzem
     return render(request, 'users/register.html', {'form': form})
 
+
+def logout_view(request):
+    return render(request, 'users/logout.html')
+
+
+@login_required
 def home(request):
     return render(request, 'index.html')
+
 
 @login_required
 def profile(request):
     return render(request, 'users/profile.html')
+
+
+@staff_member_required
+def user_list(request):
+    users = User.objects.all()
+    return render(request, 'user_list.html', {'users': users})
